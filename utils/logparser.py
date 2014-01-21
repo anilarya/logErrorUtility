9ce7f1675ebbf7322caefa6c8f4c00cb26cf7111
anilarya <anil.kumar@hashedin.com>
2014-01-21 18:48:28 +0530

    Changed config file for hadling regex inside config file

--- utils/logparser.py
+++ utils/logparser.py
@@ -9,11 +9,6 @@ import reports
 import settings
 
 
-def handle(*args, **options):
-    path =  args[0][0] 
-    recipient_email = args[0][1]
-    get_error_notification(recipient_email, path) 
-
 def lines_from_dir(filepat, dirname):
     names =  find_files(filepat,dirname)
     files =  open_files(names)
@@ -48,55 +43,39 @@ def grep(pat,lines):
     for line in lines:
         if patc.search(line): yield line
 
-def get_apache_regex():
-    expression = [
-    r'(?P<host>\S+)',                   # host %h
-    r'\S+',                             # indent %l (unused)
-    r'(?P<user>\S+)',                   # user %u
-    r'\[(?P<timestamp>.+)\]',                # timestamp %t
-    r'"(?P<request>.+)"',               # request "%r"
-    r'(?P<status>[0-9]+)',              # status %>s
-    r'(?P<size>\S+)',                   # size %b (careful, can be '-')
-    r'"(?P<referer>.*)"',               # referer "%{Referer}i"
-    r'"(?P<agent>.*)"',                 # user agent "%{User-agent}i"
-]  
-    return expression
+def get_log_re():
+    if settings.app_server == "apache" :
+        reg_expression = settings.apache_regex
+    elif settings.app_server == 'ngnix' :
+        reg_expression = settings.ngnix_regex
+    log_re = re.compile(reg_expression)
+    return log_re
 
 def get_error_notification(recipient_email, path):
     ''' A function to parse logs  and return count_dictionary of log errors '''
     wwwlog = lines_from_dir('*.log1',path)
-    reg_expression = get_apache_regex()
-    log_re = re.compile(r'\s+'.join(reg_expression)+r'\s*\Z')
     count_dict = reports.get_count_dict() 
     fd, temp_filename = reports.get_temp_file()
     c = 0
+    log_re = get_log_re()
     for line in wwwlog : 
-        c = c + 1 
-        m = log_re.match(line)  
+        c = c + 1
+        m = log_re.match(line)
         if m : 
             request = m.groupdict()
-            print request
             if c == 1:
-                time = request["timestamp"].split(":")[0]
+                time = request["request"].split(":")[0]
             count_dict, fd = reports.get_count_information(count_dict, fd, request) 
     os.close(fd)    
     if c == 0 : 
         print "No logs found" 
-        return 
+        return
     content,subject = reports.report_generate(count_dict, time)
     reports.send_report(recipient_email, content, subject, temp_filename)
 
     
 if __name__ == "__main__":
-    config =  settings.config 
-    status = config.get("config_use", "status")
-    if status == "no":
-        handle(sys.argv[1:])
-    else:
-        mode = config.get("mode", "m")
-        if mode == "local": 
-            recipient_email = config.get("local", "mailto")
-            path = config.get("local", "logpath")
-            get_error_notification(recipient_email, path)
-        else : 
-            pass
+    if settings.mode == "local": 
+        get_error_notification(settings.recipient_email, settings.path)
+    else : 
+        pass
--- utils/settings.py
+++ utils/settings.py
@@ -2,5 +2,13 @@ import os
 import ConfigParser
 
 PROJECT_DIR = os.path.split(os.path.split(os.path.abspath(__file__))[0])[0]
+
 config = ConfigParser.RawConfigParser(allow_no_value=True)
 config.read([os.path.abspath(PROJECT_DIR + '/utils/log_config.cfg')])
+status = config.get("config_use", "status")
+mode = config.get("mode", "m")
+app_server = config.get("mode", "app")
+recipient_email = config.get("local", "mailto")
+path = config.get("local", "logpath")
+apache_regex = config.get("app_web_server", "apache")
+ngnix_regex = config.get("app_web_server", "ngnix")
